c2ac986c4bfc2298470c187f533c56635210689a
marwin27 <abaquinmarwinc@gmail.com>
2026-07-21 23:09:58 +0800

    style: improved login page layout

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,12 +1,22 @@
-import react from "react";
-import { login } from "../api/authApi";
+import { useState } from "react";
+import { Box, Typography, Stack, Paper } from "@mui/material";
+import AccessTimeIcon from "@mui/icons-material/AccessTime";
+import GroupsIcon from "@mui/icons-material/Groups";
+import AssessmentIcon from "@mui/icons-material/Assessment";
+import LoginForm from "../components/auth/LoginForm";
+import { useAuth } from "../components/context/AuthContext";
+import { login as loginApi } from "../api/authApi";
+import { useNavigate } from "react-router-dom";
 
 const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-  const [email, setEmail] = react.useState("");
-  const [password, setPassword] = react.useState("");
-  const [loading, setLoading] = react.useState(false);
-  const [error, setError] = react.useState("");
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState("");
+  const { login } = useAuth();
+  const navigate = useNavigate();
+
   const handleLogin = async (e) => {
     e.preventDefault();
 
@@ -14,45 +24,140 @@ const Login = () => {
       setLoading(true);
       setError("");
 
-      const data = await login({
+      const data = await loginApi({
         email,
         password,
       });
 
-      console.log(data);
-
-      localStorage.setItem("token", data.token);
+      login(data);
+      navigate("/dashboard");
     } catch (error) {
       setError(error.response?.data?.message || "Login failed");
     } finally {
       setLoading(false);
     }
-
   };
+
   return (
-       <form onSubmit={handleLogin}>
-    {error && <p>{error}</p>}
+    <Box
+      sx={{
+        height: "100vh",
+        display: "flex",
+        bgcolor: "black",
+        background: "#0E3386",
+      }}
+    >
+      <Box
+        sx={{
+          flex: 1,
+          display: "flex",
+          justifyContent: "center",
+          flexDirection: "column",
+          px: 24,
+          color: "white",
+        }}
+      >
+        <Typography variant="h3" fontWeight="bold">
+          Employee Management System
+        </Typography>
+        <Typography
+          variant="h6"
+          color="text.secondary"
+          sx={{
+            mt: 2,
+            mb: 5,
+            maxWidth: 500,
+          }}
+        >
+          A complete solution for managing employee attendance, reports, and
+          workforce information in one secure platform.
+        </Typography>
+        <Stack spacing={2}>
+          <Paper
+            elevation={2}
+            sx={{
+              p: 3,
+              borderRadius: 3,
+              display: "flex",
+              alignItems: "center",
+              gap: 2,
+            }}
+          >
+            <AccessTimeIcon color="primary" fontSize="large" />
 
-    <input
-      type="email"
-      placeholder="Email"
-      value={email}
-      onChange={(e) => setEmail(e.target.value)}
-    />
+            <Box>
+              <Typography fontWeight="bold">Attendance Tracking</Typography>
 
-    <input
-      type="password"
-      placeholder="Password"
-      value={password}
-      onChange={(e) => setPassword(e.target.value)}
-    />
+              <Typography variant="body2" color="text.secondary">
+                Track employee time-in, time-out, overtime and lateness.
+              </Typography>
+            </Box>
+          </Paper>
+          <Paper
+            elevation={2}
+            sx={{
+              p: 3,
+              borderRadius: 3,
+              display: "flex",
+              alignItems: "center",
+              gap: 2,
+            }}
+          >
+            <GroupsIcon color="primary" fontSize="large" />
 
-    <button type="submit" disabled={loading}>
-      {loading ? "Logging in..." : "Login"}
-    </button>
-  </form>
+            <Box>
+              <Typography fontWeight="bold">Employee Management</Typography>
 
+              <Typography variant="body2" color="text.secondary">
+                Employee Management Manage employee information, departments and
+                positions.
+              </Typography>
+            </Box>
+          </Paper>
+          <Paper
+            elevation={2}
+            sx={{
+              p: 3,
+              borderRadius: 3,
+              display: "flex",
+              alignItems: "center",
+              gap: 2,
+            }}
+          >
+            <AssessmentIcon color="primary" fontSize="large" />
+
+            <Box>
+              <Typography fontWeight="bold">Reports and Analytics</Typography>
+
+              <Typography variant="body2" color="text.secondary">
+                Reports & Analytics Generate monthly attendance and performance
+                reports.
+              </Typography>
+            </Box>
+          </Paper>
+        </Stack>
+      </Box>
+      <Box
+        sx={{
+          flex: 1,
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          px: 24,
+        }}
+      >
+        <LoginForm
+          email={email}
+          password={password}
+          loading={loading}
+          error={error}
+          setEmail={setEmail}
+          setPassword={setPassword}
+          handleLogin={handleLogin}
+        />
+      </Box>
+    </Box>
   );
 };
 
-export default Login
+export default Login;
