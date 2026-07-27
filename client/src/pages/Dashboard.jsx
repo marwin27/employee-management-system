f0abeaa38e19eba11b4d80dd44a47f9d0062256d
marwin27 <abaquinmarwinc@gmail.com>
2026-07-28 07:02:10 +0800

    feat: enhance dashboard with attendance analytics

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,10 +1,23 @@
-import { Box, Typography, Grid, Card, CardContent, Stack } from "@mui/material";
+import {
+  Box,
+  Typography,
+  Grid,
+  Card,
+  CardContent,
+  Stack,
+  Chip,
+} from "@mui/material";
 import GroupsIcon from "@mui/icons-material/Groups";
 import CheckCircleIcon from "@mui/icons-material/CheckCircle";
 import CancelIcon from "@mui/icons-material/Cancel";
 import AccessTimeIcon from "@mui/icons-material/AccessTime";
+import ScheduleIcon from "@mui/icons-material/Schedule";
 import { useState, useEffect } from "react";
-import { getAttendanceSummary } from "../api/attendanceApi";
+import {
+  getAttendanceSummary,
+  getMonthlyAttendance,
+  getRecentAttendance,
+} from "../api/attendanceApi";
 import { useAuth } from "../components/context/AuthContext";
 import {
   ResponsiveContainer,
@@ -15,10 +28,24 @@ import {
   Tooltip,
   CartesianGrid,
 } from "recharts";
+import dayjs from "dayjs";
 
 const Dashboard = () => {
   const [summary, setSummary] = useState(null);
-  const { token } = useAuth();
+  const { token, user } = useAuth();
+
+  const formatMinutes = (minutes) => {
+    if (!minutes || minutes <= 0) return "0 mins";
+
+    const hours = Math.floor(minutes / 60);
+    const mins = minutes % 60;
+
+    if (hours === 0) return `${mins} min${mins > 1 ? "s" : ""}`;
+    if (mins === 0) return `${hours} hr${hours > 1 ? "s" : ""}`;
+
+    return `${hours} hr${hours > 1 ? "s" : ""} ${mins} min${mins > 1 ? "s" : ""}`;
+  };
+
   const stats = [
     {
       title: "Total Attendance",
@@ -45,26 +72,14 @@ const Dashboard = () => {
       value: summary?.halfDay || 0,
       icon: <AccessTimeIcon sx={{ fontSize: 40 }} />,
     },
-  ];
-
-  const chartData = [
     {
-      name: "Present",
-      value: summary?.present || 0,
-    },
-    {
-      name: "Absent",
-      value: summary?.absent || 0,
-    },
-    {
-      name: "Late",
-      value: summary?.late || 0,
-    },
-    {
-      name: "Half Day",
-      value: summary?.halfDay || 0,
+      title: "Total Overtime",
+      value: formatMinutes(summary?.totalOvertimeMinutes),
+      icon: <ScheduleIcon sx={{ fontSize: 40 }} />,
     },
   ];
+  const [monthlyAttendance, setMonthlyAttendance] = useState([]);
+  const chartData = monthlyAttendance;
 
   const fetchSummary = async () => {
     try {
@@ -75,11 +90,58 @@ const Dashboard = () => {
     }
   };
 
+  const fetchMonthlyAttendance = async () => {
+    try {
+      const data = await getMonthlyAttendance(token);
+      console.log(data);
+      const monthNames = [
+        "",
+        "Jan",
+        "Feb",
+        "Mar",
+        "Apr",
+        "May",
+        "Jun",
+        "Jul",
+        "Aug",
+        "Sep",
+        "Oct",
+        "Nov",
+        "Dec",
+      ];
+
+      const formattedData = data.map((item) => ({
+        month: monthNames[item._id.month],
+        Present: item.present,
+        Late: item.late,
+        Absent: item.absent,
+        "Half Day": item.halfDay,
+      }));
+
+      setMonthlyAttendance(formattedData);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const [recentAttendance, setRecentAttendance] = useState([]);
+  const fetchRecentAttendance = async () => {
+    try {
+      const data = await getRecentAttendance(token);
+      setRecentAttendance(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   useEffect(() => {
     if (token) {
       fetchSummary();
+      fetchMonthlyAttendance();
+      fetchRecentAttendance();
     }
   }, [token]);
+
   return (
     <Box sx={{ p: 3 }}>
       <Typography variant="h4" fontWeight="bold">
@@ -87,11 +149,14 @@ const Dashboard = () => {
       </Typography>
 
       <Typography color="text.secondary" sx={{ mt: 1 }}>
-        Welcome to the Employee Management System.
+        Welcome back, <strong>{user?.firstName}</strong> 👋
+      </Typography>
+      <Typography color="text.secondary" sx={{ mt: 1 }}>
+        Here's your attendance summary for today.
       </Typography>
       <Grid container spacing={3} sx={{ mt: 2 }}>
         {stats.map((stat) => (
-          <Grid size={{ xs: 12, sm: 6, md: 3 }} key={stat.title}>
+          <Grid size={{ xs: 12, sm: 6, md: 4 }} key={stat.title}>
             <Card sx={{ height: "100%" }}>
               <CardContent>
                 <Stack direction="row" spacing={2} alignItems="center">
@@ -138,10 +203,14 @@ const Dashboard = () => {
                 <ResponsiveContainer width="100%" height="100%">
                   <BarChart data={chartData}>
                     <CartesianGrid strokeDasharray="3 3" />
-                    <XAxis dataKey="name" />
+                    <XAxis dataKey="month" />
                     <YAxis />
                     <Tooltip />
-                    <Bar dataKey="value" fill="#1976d2" radius={[6, 6, 0, 0]} />
+
+                    <Bar dataKey="Present" fill="#4caf50" />
+                    <Bar dataKey="Absent" fill="#9e9e9e" />
+                    <Bar dataKey="Late" fill="#ff9800" />
+                    <Bar dataKey="Half Day" fill="#f44336" />
                   </BarChart>
                 </ResponsiveContainer>
               </Box>
@@ -155,16 +224,46 @@ const Dashboard = () => {
                 Recent Attendance
               </Typography>
 
-              <Box
-                sx={{
-                  height: 300,
-                  display: "flex",
-                  justifyContent: "center",
-                  alignItems: "center",
-                }}
-              >
-                Table Coming Soon
-              </Box>
+              <Stack spacing={2} sx={{ mt: 2 }}>
+                {recentAttendance.map((record) => (
+                  <Box
+                    key={record._id}
+                    sx={{
+                      display: "flex",
+                      justifyContent: "space-between",
+                      alignItems: "center",
+                      borderBottom: "1px solid #eee",
+                      pb: 1,
+                    }}
+                  >
+                    <Box>
+                      <Typography fontWeight="bold">
+                        {dayjs(record.date).format("MMM D, YYYY")}
+                      </Typography>
+
+                      <Typography variant="body2" color="text.secondary">
+                        {dayjs(record.timeIn).format("h:mm A")}
+                        {record.timeOut &&
+                          ` - ${dayjs(record.timeOut).format("h:mm A")}`}
+                      </Typography>
+                    </Box>
+
+                    <Chip
+                      label={record.status}
+                      color={
+                        record.status === "Present"
+                          ? "success"
+                          : record.status === "Late"
+                            ? "warning"
+                            : record.status === "Half Day"
+                              ? "error"
+                              : "default"
+                      }
+                      size="small"
+                    />
+                  </Box>
+                ))}
+              </Stack>
             </CardContent>
           </Card>
         </Grid>
