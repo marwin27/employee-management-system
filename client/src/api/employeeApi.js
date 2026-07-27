45a51a33c3f220b3f696c240872a143ae7412bda
marwin27 <abaquinmarwinc@gmail.com>
2026-07-28 06:43:11 +0800

    feat: implemented employee update and deactivation API

--- client/src/api/employeeApi.js
+++ client/src/api/employeeApi.js
@@ -21,3 +21,27 @@ export const getEmployees = async (token) => {
 
   return response.data;
 };
+
+export const updateEmployee = async (id, employeeData, token) => {
+  const response = await axios.put(`${API_URL}/${id}`, employeeData, {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+
+  return response.data;
+};
+
+export const deactivateEmployee = async (id, token) => {
+  const response = await axios.put(
+    `${API_URL}/${id}/deactivate`,
+    {},
+    {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    },
+  );
+
+  return response.data;
+};
