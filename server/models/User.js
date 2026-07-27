21f05ebdee7df18dcda78501f4de036d18531eb5
marwin27 <abaquinmarwinc@gmail.com>
2026-07-28 06:37:41 +0800

    refactor: use PascalCase for User role

--- server/models/User.js
+++ server/models/User.js
@@ -26,8 +26,8 @@ const userSchema = new mongoose.Schema(
     },
     role: {
       type: String,
-      enum: ["employee", "admin"],
-      default: "employee",
+      enum: ["Employee", "Admin"],
+      default: "Employee",
     },
     department: String,
 
